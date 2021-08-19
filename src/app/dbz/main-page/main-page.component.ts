8ee6164720a5eb37fcaaa39a2bf46c23e3b20fa4
Cristhian David <crisdavid323@gmail.com>
2021-08-18 20:28:27 -0500

    Deputación del proyecto

---
+++ src/app/dbz/main-page/main-page.component.ts
@@ -0,0 +1,31 @@
+import { Component, Input } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html',
+  styleUrls: ['./main-page.component.css']
+})
+
+export class MainPageComponent {
+
+  personajes: Personaje[] = [
+    {
+      nombre: 'Goku',
+      poder: 15000
+    },
+    {
+      nombre: 'Vegeta',
+      poder: 14000
+    }
+  ];
+  nuevo: Personaje = {
+    nombre: '',
+    poder: 0
+  }
+
+  agregarNuevoPersonaje(argumento: Personaje) {
+    this.personajes.push(argumento);
+  }
+
+}
